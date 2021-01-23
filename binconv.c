f9b67b4e3740b301d66a749b7504b24c3e94908f
viviaaan <viviansamuel42@gmail.com>
2021-01-23 17:16:33 +0530

    make lots of changes
    I removed the part where the program displays the number with its
    output because I realized that I won't use/like it. I changed
    up the error message to be more precise. Also changed some loop
    styles and conditions.

--- binconv.c
+++ binconv.c
@@ -1,43 +1,47 @@
 /* Convert a number to binary */
 #include <stdio.h>
 #include <stdlib.h>
-void convert(int inp);
+int convert(int inp);
 
 int main(int argc, char *argv[])
 {
-    int inp = 0;
+    int inp;
     if (argc == 1) {
         printf("Enter a whole number : ");
         scanf(" %d", &inp);
-        convert(inp);
+        if (convert(inp))
+            printf("error: %d is not a whole number\n", inp);
     }
-    else {
+    else
         for (int j = 1; j < argc; j++) {
             inp = atoi(argv[j]);
-            printf("%s = ", argv[j]);
-            convert(inp);
+            if (convert(inp))
+                printf("error: %s is not a whole number\n", argv[j]);
         }
-    }
 }
 
-void convert(int inp)
+int convert(int inp)
 {
+    if (inp < 0)
+        return (-1);
+    else {
     int tmp = inp;
     int i = 0;
-    if (inp < 0)
-        printf("error: argument must be a positive integer");
+
     while (tmp /= 2)
         i++; // `i` equals the largest power of 2 that is less than or equal to `tmp`
 
-    char result[i];
-    result[i-1] = '\0';
+    char string[i];
 
-    for (i = 0; inp >= 1; i++) {
-        result[i] = (inp % 2) + '0';
-        inp /= 2;
-    }
+    i = 0;
+    do
+        string[i++] = (inp % 2) + '0';
+    while (inp /= 2);
 
     while (i) // print the char array backwards
-        putchar(result[--i]);
+        putchar(string[--i]);
     putchar('\n');
+
+    return 0;
+    }
 }
