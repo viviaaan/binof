246c481e8a889c463563ba6d6b09b4c4b9d741de
viviaaan <viviansamuel42@gmail.com>
2021-02-22 18:23:31 +0530

    chang a loop

--- binconv.c
+++ binconv.c
@@ -25,12 +25,10 @@ int convert(int inp)
     if (inp < 0)
         return (-1);
     else {
-    int tmp = inp;
     int i = 0;
 
-    while (tmp /= 2)
-        i++; // `i` equals the largest power of 2 that is less than or equal to `tmp`
-
+    for (int tmp = inp; tmp /= 2; i++)
+        ;
     char string[i];
 
     i = 0;
