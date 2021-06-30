70964e2f4d256c41c014537415a128db8521311b
viviaaan <viviansamuel42@gmail.com>
2021-06-30 16:35:00 +0530

    add interactive mode

--- twocomp.c
+++ twocomp.c
@@ -3,34 +3,36 @@
 #include <stdlib.h>
 char* toBits(int num);
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     int input;
+    char* ans;
 
     if (argc == 1) {
-        printf("Enter an integer: ");
+       do {
+        printf("Integer: ");
         scanf(" %d", &input);
-        printf("%s", toBits(input));
-        putchar('\n');
+        ans = toBits(input);
+        printf("%s\n", ans);
+        free(ans);
+      } while(getchar() != EOF);
     }
     else
         for (int j = 1; j < argc; j++) {
             input = atoi(argv[j]);
-            printf("%s", toBits(input));
-            putchar('\n');
-
+            ans = toBits(input);
+            printf("%s\n", ans);
+            free(ans);
         }
 }
 
-char* toBits(int num)
-{
+char* toBits(int num) {
     int highest, tmp;
     highest = tmp = 0;
 
-    for (tmp = num; tmp /= 2; highest++) // find the highest power of 2 less than num
+    for (tmp = num; tmp /= 2; highest++) // find the highest power of 2 that is less than num
         ;
     char tmpstr[highest];
-    char* bitstr = (char*)malloc(sizeof(tmpstr)+1);
+    char* bitstr = (char*)malloc(sizeof(tmpstr)+1); // this should be freed after usage by the caller
 
     if (num < 0) // figure out MSB
       bitstr[0] = '1';
@@ -48,5 +50,4 @@ char* toBits(int num)
         bitstr[j++] = tmpstr[--i]; // reversing tmpstr into bitstr
 
     return(bitstr);
-    free(bitstr);
 }
